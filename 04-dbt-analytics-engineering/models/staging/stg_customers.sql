c354ef7cc152d8b8a7dbc3cb531edf1c4dd618cc
WAWA012 <jihankusumawwardhani@gmail.com>
2026-09-12 19:34:54 +0700

    Handle empty dates in dbt staging models

--- 04-dbt-analytics-engineering/models/staging/stg_customers.sql
+++ 04-dbt-analytics-engineering/models/staging/stg_customers.sql
@@ -10,6 +10,6 @@ select
     customer_id::integer          as customer_id,
     trim(customer_name)           as customer_name,
     lower(trim(email))            as email,
-    signup_date::date             as signup_date,
+    try_cast(nullif(trim(signup_date), '') as date) as signup_date,
     country                       as country
 from source
--- 04-dbt-analytics-engineering/models/staging/stg_payments.sql
+++ 04-dbt-analytics-engineering/models/staging/stg_payments.sql
@@ -5,7 +5,7 @@ with source as (
 select
     payment_id::integer                        as payment_id,
     subscription_id::integer                    as subscription_id,
-    payment_date::date                          as payment_date,
+    try_cast(nullif(trim(payment_date), '') as date) as payment_date,
     {{ cents_to_dollars('amount_cents') }}      as amount_usd,
     status                                       as payment_status
 from source
